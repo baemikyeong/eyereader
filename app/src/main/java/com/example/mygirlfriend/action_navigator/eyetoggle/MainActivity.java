d967e91419165a4041af55bb9ff6f34482d0d2d5
skydayeon <swdy1233@gmail.com>
2017-06-04 03:46:13 +0900

    recorder ㅜㅜ

--- app/src/main/java/com/example/mygirlfriend/action_navigator/eyetoggle/MainActivity.java
+++ app/src/main/java/com/example/mygirlfriend/action_navigator/eyetoggle/MainActivity.java
@@ -3,10 +3,8 @@ package com.example.mygirlfriend.action_navigator.eyetoggle;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.SwitchCompat;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,19 +12,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mygirlfriend.action_navigator.R;
 import com.example.mygirlfriend.action_navigator.eyetoggle.tracker.FaceTracker;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.face.FaceDetector;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -39,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private double left_thres = 0;
     private double right_thres = 0;
     private Button textbutton;
-
+    private boolean isRecording = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -127,7 +120,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.action_bookmark) {
 
         } else if (id == R.id.action_mic) {
-
+            if(isRecording == false) {
+                startService(new Intent(this, AudioService.class));
+                isRecording = true;
+            }
+            else{
+                stopService(new Intent(this,AudioService.class));
+                isRecording = false;
+            }
         } else if (id == R.id.action_initialize) {
             Intent intent = new Intent(MainActivity.this, FaceTrackerActivity.class);
 
